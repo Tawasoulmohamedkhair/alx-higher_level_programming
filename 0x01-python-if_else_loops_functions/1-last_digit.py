abe3bcb3f344ec9222c62ca50041c301888ca87f
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2023-01-03 13:30:40 -0800

    Task1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ if number < 0:
 if digit > 5:
     print("and is greater than 5")
 elif digit == 0:
-    print("and  is 0")
+    print("and is 0")
 else:
-    print("and is  less than 6 and not 0")
+    print("and is less than 6 and not 0")
