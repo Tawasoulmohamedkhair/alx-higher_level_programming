a9f68fcd1c9bfa9e7817fb6e02ce2527724f8c00
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2023-01-08 04:42:01 -0800

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,15 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(j), end="")
             else:
                 print("{:d}".format(j), end=" ")
-                l = l + 1
-                print()
+            l = l + 1
+        print()#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        l = 1
+        for j in i:
+            if l == len(i):
+                print("{:d}".format(j), end="")
+            else:
+                print("{:d}".format(j), end=" ")
+            l = l + 1
+        print()
