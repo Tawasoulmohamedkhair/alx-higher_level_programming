e1abf13a914fd144ce7568799669c0da05211e70
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2023-01-12 04:05:08 -0800

    Task13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,9 @@
-#!/usr/bin/pythoni3
+#!/usr/bin/python3
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    average = 0
-    div = 0
-    for tup in my_list:
-        average += tup[0] * tup[1]
-        div += tup[1]
-    return float(average / div)
+    res = 0.0
+    s_list = list(t[0] * t[1] for t in my_list)
+    w_list = list(t[1] for t in my_list)
+    res = sum(s_list) / sum(w_list)
+    return res
