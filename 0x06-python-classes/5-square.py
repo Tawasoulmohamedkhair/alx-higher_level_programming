b1f96fa843b1173f801e9052920ebf870498e309
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2023-01-24 14:53:11 -0800

    Task5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,60 +1,38 @@
 #!/usr/bin/python3
-"""class Square that defines a square"""
+"""Defines a class Square."""
 
 
-class Square():
-    """square class with it's size and proper validation"""
+class Square:
+    """Represents a square."""
 
-    def __init__(self, size=0):
-        self.__size = size
+    def __init__(self, size):
+        """Initializes a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
     @property
     def size(self):
-        return self.__size
+        """Gets/sets the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        if (type(value) is not int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif (value < 0):
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        return self.__size ** 2
+        """Returns the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        if not self.__size:
+        """Prints the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
             print("")
-        for i in range(self.__size):
-            print("#" * self.__size)
-#!/usr/bin/python3
-"""class Square that defines a square"""
-
-
-class Square():
-    """square class with it's size and proper validation"""
-
-    def __init__(self, size=0):
-        self.__size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if (type(value) is not int):
-            raise TypeError("size must be an integer")
-        elif (value < 0):
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        return self.__size ** 2
-
-    def my_print(self):
-        if not self.__size:
+        if self.__size == 0:
             print("")
-        for i in range(self.__size):
-            print("#" * self.__size)
